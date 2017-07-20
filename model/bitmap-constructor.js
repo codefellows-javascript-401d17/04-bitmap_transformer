38c53605f11c1525ed3d5d7613fe8b579ee21735
Nick Asmussen <nick.asmussen1@gmail.com>
2017-07-20 16:44:41 -0700

    adds color table to the bitmap constructor

--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -10,12 +10,14 @@ const bitObj = {};
 function BitConstructor(data){
   this.type = data.toString('utf-8', 0, 2);
   this.size = data.readInt32LE(2);
+  this.pixelArrayStart = data.readInt32LE(10);
   this.headerSize = data.readInt32LE(14);
   this.bitsPerPixel = data.readInt32LE(28);
   this.width = data.readInt32LE(18);
   this.height = data.readInt32LE(22);
   // this.maxColorNum = data.readInt32LE(46);
   this.colorTableStart = this.headerSize + 14;
+  this.colorTable = data.toString('hex', this.colorTableStart, this.pixelArrayStart);
 
   this.rowWidth = Math.ceil(((this.bitsPerPixel * this.width + 31)/32) * 4);
 
