ce2f4367b6555ff5eea4ef05dd2898a9d5be9289
ohjonah <ohjonah@gmail.com>
2017-07-21 13:36:30 -0700

    Adds Tests

--- test/bitmap-file-helper-test.js
+++ test/bitmap-file-helper-test.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const expect = require('chai').expect;
+const bitmapFileHelper = require(`${__dirname}/../lib/bitmap-file-helper.js`);
+
+describe('Bitmap File Reader', function() {
+    describe('with improper file path', () => {
+        it('should return an error', done => {
+            bitmapFileHelper(`${__dirname}/not-a-file.bmp`, err => {
+                expect(err).to.be.an('error');
+                done();
+            });
+        });
+    });
+});
