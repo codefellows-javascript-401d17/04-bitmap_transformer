cb8cc1b4fde0386b60ebad3c417822bfddc8d3d7
Loaye <jr.carstens00@gmail.com>
2017-07-21 15:42:51 -0700

    scaffold

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const fs = require('fs');
+
+var read = require(`${__dirname}/lib/read-file.js`);
+var write = require(`${__dirname}/lib/write-file.js`);
+var transform = require(`${__dirname}/lib/transform.js`);
--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -4,16 +4,24 @@ const fs = require('fs');
 
 //Gets bitmap data and put into construtor
 const Bitmap = module.exports = function(data) {
+  this.data = data;
   this.id = data.toString('utf-8', 0, 2);
   this.size = data.readUInt32LE(2);
   this.height = data.readUInt32LE(18);
   this.width = data.readUInt32LE(22);
   this.pixelOffSet = data.readUInt32LE(10);
   this.colorArray = data.readUInt32LE(54, this.pixelOffSet);
-  this.data = data;
 };
 
+//blackout
+Bitmap.prototype.blackOut = function() {
+  this.colorArray.fill(0);
+};
 
+//reverse
+Bitmap.prototype.reversed = function() {
+  this.pixelOffSet.reverse();
+};
 
 
 
