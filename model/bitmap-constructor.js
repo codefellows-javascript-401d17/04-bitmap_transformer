417af6668449205c27fbbee21db6b5fdf9493c1a
Allan Liebold <allan.liebold@gmail.com>
2017-07-21 16:06:49 -0700

    creates bitmap object constructor

--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -1,1 +1,8 @@
 'use strict';
+
+module.exports = exports = {};
+
+exports.Bitmap = function(data) {
+  this.name = data.toString('hex', 0, 2);
+  console.log(data);
+};
--- test/bitmap-constructor-test.js
+++ test/bitmap-constructor-test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const expect = require('chai').expect;
+const bitmapConstructor = require(`${__dirname}/../model/bitmap-constructor.js`);
+
+describe('Bitmap Constructor', function() {
+  describe('if not an object', () => {
+      it('should throw not an object error', done => {
+        let result = bitmapConstructor();
+        expect(result).to.throw(Error);
+        done();
+      });
+  });
+  describe('if object', () => {
+    it('should return new Bitmap object', done => {
+      let result = bitmapConstructor();
+      expect(result).to.be.an('object');
+    });
+  });
+});
