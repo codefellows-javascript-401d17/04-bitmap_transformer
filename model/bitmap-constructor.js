a7d3ebbef6df49fa64fbf9462000cb70762d566a
Allan Liebold <allan.liebold@gmail.com>
2017-07-21 16:28:32 -0700

    adds properties to bitmap object constructor for color and pixel arrays.

--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -3,6 +3,14 @@
 module.exports = exports = {};
 
 exports.Bitmap = function(data) {
-  this.name = data.toString('hex', 0, 2);
-  console.log(data);
+  // this.all = data.toString('hex');
+
+  this.filetype = data.toString('hex', 0, 2);
+  this.offsetToPixelArr = data.readInt32LE(10);
+
+  this.infoHeader = data.readInt32LE(14);
+  this.offsetToColorArr = this.infoHeader + 14;
+
+  this.colorArr = data.toString('hex', this.offsetToColorArr, 182);
+  this.pixelArr = data.toString('hex', this.offsetToPixelArr);
 };
