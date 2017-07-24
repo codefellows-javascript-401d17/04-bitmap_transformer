1bd033df65443882f8045f2c5ca2dd5c9db8b4b8
ohjonah <ohjonah@gmail.com>
2017-07-24 12:41:30 -0700

    Refactors Constructor and Helper Tests for Test Cases

--- test/bitmap-constructor-test.js
+++ test/bitmap-constructor-test.js
@@ -2,19 +2,20 @@
 
 const expect = require('chai').expect;
 const bitmapConstructor = require(`${__dirname}/../model/bitmap-constructor.js`);
+const bitmapFileHelper = require(`${__dirname}/../lib/bitmap-file-helper.js`);
 
 describe('Bitmap Constructor', function() {
-  describe('if not an object', () => {
-      it('should throw not an object error', done => {
-        let result = bitmapConstructor();
-        expect(result).to.throw(Error);
-        done();
-      });
-  });
   describe('if object', () => {
     it('should return new Bitmap object', done => {
-      let result = bitmapConstructor();
-      expect(result).to.be.an('object');
+      bitmapFileHelper(`${__dirname}/../assets/palette-bitmap.bmp`, function(err, data) {
+        if(err) throw err;
+        let result = new bitmapConstructor.Bitmap(data);
+        expect(result).to.be.an('object');
+        done();
+      });
     });
   });
 });
+
+
+
