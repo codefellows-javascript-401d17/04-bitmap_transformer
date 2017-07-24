2823012da1bfdf090012e37222a127a4ebb7b126
ohjonah <ohjonah@gmail.com>
2017-07-23 18:16:11 -0700

    Cleans up Code

--- index.js
+++ index.js
@@ -6,33 +6,29 @@ const greyScaleTransform = require(`${__dirname}/lib/grey-scale-transform.js`);
 const reverseTransform = require(`${__dirname}/lib/reverse-transform.js`);
 const invertTransform = require(`${__dirname}/lib/invert-transform.js`);
 
-// GREYSCALE: This will be called in index.js
+
 bitmapFileHelper(`${__dirname}/assets/palette-bitmap.bmp`, function(err, data) {
   if(err) throw err;
-  var bmp = new bitmapConstructor.Bitmap(data);
-  bmp.colorArr = greyScaleTransform(bmp.colorArr);
+  let bmp = new bitmapConstructor.Bitmap(data);
 
+  bmp.colorArr = greyScaleTransform(bmp.colorArr);
   bmp.newFile('greyscaled');
 });
 
-// REVERSE: This will be called in index.js
 bitmapFileHelper(`${__dirname}/assets/palette-bitmap.bmp`, function(err, data) {
   if(err) throw err;
-  var bmp = new bitmapConstructor.Bitmap(data);
+  let bmp = new bitmapConstructor.Bitmap(data);
 
   bmp.pixelArr = reverseTransform(bmp.pixelArr);
-
   bmp.newFile('reversed');
 });
 
-// INVERT: This will be called in index.js
+
 bitmapFileHelper(`${__dirname}/assets/palette-bitmap.bmp`, function(err, data) {
   if(err) throw err;
-  var bmp = new bitmapConstructor.Bitmap(data);
-  // console.log('colorArr:', bmp.colorArr.length);
+  let bmp = new bitmapConstructor.Bitmap(data);
 
   bmp.colorArr = invertTransform(bmp.colorArr);
-
   bmp.newFile('inverted');
 });
 
--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -6,13 +6,10 @@ const fs = require('fs');
 
 exports.Bitmap = function(data) {
   this.all = data;
-
   this.filetype = data.toString('hex', 0, 2);
   this.offsetToPixelArrInHex = data.toString('hex', 10, 14);
   this.offsetToPixelArr = data.readInt32LE(10);
-
   this.offsetToColorArr = 54;
-
 
   // full file:
   this.headerAll = data.toString('hex', 0, this.offsetToColorArr);
